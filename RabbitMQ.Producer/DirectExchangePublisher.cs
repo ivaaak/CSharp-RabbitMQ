f1d50e3fdedc4efb213f5bb17de7dc6b3f7d4fc9
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-08-15 18:03:09 +0300

    Added direct exchange producer

---
+++ RabbitMQ.Producer/DirectExchangePublisher.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQ.Producer
+{
+    public class DirectExchangePublisher
+    {
+        public static void Publish(IModel channel)
+        {
+            var timeToLeave = new Dictionary<string, object>
+            {
+                { "x-message-ttl", 3000 } //time to leave in ms
+            };
+
+            channel.ExchangeDeclare(
+               "demo-direct-exchange", 
+               ExchangeType.Direct, 
+               arguments: timeToLeave);
+
+            var count = 0;
+
+            while (true)
+            {
+                var message = new { Name = "Producer", Message = $"Hello! Count: {count}" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+                channel.BasicPublish("demo-direct-exchange", "account.init", null, body);
+                count++;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
