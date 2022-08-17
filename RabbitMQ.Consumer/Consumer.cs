b95dacc407522939c875bccad35255ef46281050
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-08-17 12:10:26 +0300

    Added Consumer

---
+++ RabbitMQ.Consumer/Consumer.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ.Consumer
+{
+    public static class Consumer
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            QueueConsumer.Consume(channel);
+        }
+    }
+}
