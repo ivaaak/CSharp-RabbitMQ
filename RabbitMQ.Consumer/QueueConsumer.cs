8c8f90ecd3c25dfc1cbafd52c75875f88ea1154c
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-08-18 10:12:38 +0300

    Added Queue Consumer

---
+++ RabbitMQ.Consumer/QueueConsumer.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ.Consumer
+{
+    public static class QueueConsumer
+    {
+        public static void Consume(IModel channel)
+        {
+            channel.QueueDeclare(
+                "demo-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: true,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, args) =>
+            {
+                var body = args.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume("demo-queue", true, consumer);
+
+            Console.WriteLine("Queue consumer started");
+            Console.ReadLine();
+        }
+    }
+}
